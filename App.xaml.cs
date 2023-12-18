e731c986b5022f6e7cb1f64e6cb4e3e215370cd6
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2023-12-18 14:22:36 +0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DzпImapPost_18_12_2023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
