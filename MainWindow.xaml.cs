9bb5947184127a1137a48fa3d64353835c628f30
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2023-12-18 15:09:14 +0700

    домашка готовая

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DzпImapPost_18_12_2023
 {
@@ -19,6 +13,51 @@ namespace DzпImapPost_18_12_2023
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public static string Code;
+        
+        // генератор случайных 6 символов
+        public static string GenRand()
+        {
+            // создаём обьект под хранение случайных чисел
+            Random rnd = new Random();
+            // создаём константную строку для выбора случайных символов
+            const string chars = "0123456789";
+            // https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings
+            return new string(Enumerable.Repeat(chars,6)
+                .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+        public static string SendPost(string userPost, string randCode)
+        {
+            // создание клиента
+            SmtpClient smtpClient = new SmtpClient("smtp.mail.ru");
+            // номер порта
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;                   //     ящик           временный пароль( вторичный пароль)
+            smtpClient.Credentials = new NetworkCredential("A_Fiscman@list.ru", "ittEHuHPF0TDK19ZqAT2");
+            // отправка сообщения     откуда        куда        заголовок    тело
+            smtpClient.Send("A_Fiscman@list.ru", userPost, "код для регистрации", randCode);
+            return randCode;
+        }
+        
+        private void buttSend_Click(object sender, RoutedEventArgs e)
+        {
+            // берем ящик введенный пользователем
+            string userPost = textboxPost.Text;
+            // 
+            string randCode = GenRand();
+            SendPost(userPost,randCode);
+            Code = randCode;
+            
+        }
+
+        private void buttReg_Click(object sender, RoutedEventArgs e)
+        {
+            // проверяем код введенный пользователем 
+            if (Code == textboxPass.Text)
+            {
+                MessageBox.Show("You win");
+            }
         }
     }
 }
